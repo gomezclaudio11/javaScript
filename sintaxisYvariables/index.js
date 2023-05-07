5ba8c5bf2bf8b5d8a835599a4809d6cb4f9ecb31
gomezclaudio11 <claudiogomez23@gmail.com>
2023-05-06 23:22:27 -0300

    variables

---
+++ sintaxisYvariables/index.js
@@ -0,0 +1,10 @@
+let entrada1 = prompt ("Ingrese su nombre");
+console.log( `Hola ${entrada1}`);
+
+let entrada2 = prompt ("ingrese un numero");
+let parseEntrada2 = parseInt (entrada2);
+let numeroFijo = 5;
+let suma = parseEntrada2 + numeroFijo
+console.log( `la suma es ${suma} `);
+
+
