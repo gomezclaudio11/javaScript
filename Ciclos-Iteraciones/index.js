63acade4ad9b59d8f477087ce62b7f510e2e5e79
gomezclaudio11 <claudiogomez23@gmail.com>
2023-05-09 23:43:20 -0300

    for/while/switch

---
+++ Ciclos-Iteraciones/index.js
@@ -0,0 +1,73 @@
+// ALGORITMO PARA CALCULARLA TABLA DE MULTIPLICAR DE UN NUMERO
+/*
+let ingresarNumero = parseInt(prompt("ingresar numero"));
+
+for (let i = 1; i <= 10 ; i++) {
+    let resultado = ingresarNumero * i;
+    alert(ingresarNumero +" x "+ i +" = "+ resultado);    
+}
+
+// ALGORITMO PARA DAR TURNOS
+
+for (let i = 1; i <= 5 ; i++) {
+    let ingresarNombre = prompt("Ingrese su nombre");
+    alert("Turno N° "+ i+ " asignado a: "+ ingresarNombre);
+}
+*/
+/*
+//SENTENCIA BREAK Y CONTINUE
+
+for (let i = 1; i <= 10; i++) {
+    if (i == 5) {
+        break
+    };
+    console.log (i)
+}
+
+for (let i = 1; i <= 10; i++){
+    if (i == 5) {
+        continue
+    };
+    console.log(i);
+}
+*/
+/*
+// WHILE
+
+    let entrada = prompt (" Ingresar un dato")
+
+    while (entrada != "ESC") {
+        alert("el usuario ingreso "+ entrada);
+
+        entrada = prompt("ingresar otro dato");
+    }
+    */
+/*
+// DO WHILE
+
+let numero = 0;
+do {
+    numero = prompt ("Ingrese un numero");
+    console.log(numero);
+} while (parseInt(numero));
+*/
+
+//SWITCH
+/*
+let entrada = prompt( "Ingresar un nombre")
+while (entrada != "ESC") {
+    switch (entrada) {
+        case "ana":
+            alert("hola ana");
+            break;
+        case "JUAN":
+            alert("hola JUAN");
+            break;
+        default:
+            alert("quien sos?")
+        break;
+    }
+    entrada = prompt("ingrese otro dato")
+}
+*/
+
