af7fb808e8230b8a60f8e9346d69f80393b24182
gomezclaudio11 <claudiogomez23@gmail.com>
2023-07-01 21:28:25 -0300

    asincronias y promesas

--- 14asincroniasYpromesas/index.js
+++ 14asincroniasYpromesas/index.js
@@ -79,3 +79,94 @@ const interval = setInterval(() => {
         console.log("se elimino el intervalo xq llego a 5");
     }
 }, 1000)
+
+
+//PROMESAS
+//es un evento a futuro
+// tres estados posibles: pending, fulfilled y rejected
+new Promise ( (resolve, reject) => {
+    //CUERPO DE LA PROMESA
+})
+
+//RESOLVE AND REJECT
+const eventoFuturo = () => {
+    return new Promise ( (resolve, reject) => {
+        // cuerpo de la promesa
+    })
+}
+console.log(eventoFuturo());
+
+const eventoFuturo2 = (res) => {
+    return new Promise ( (resolve, reject) => {
+        if (res === true) {
+            resolve ("promesa resuelta")
+        } else {
+            reject ("promesa rechazada")
+        }
+    })
+}
+console.log(eventoFuturo2(true));// Promise ("promesa resuelta")
+console.log(eventoFuturo2(false));// promise ( reject "promesa rechazada")
+
+//con set time out
+
+const eventoFuturo3 = (res) => {
+    return new Promise ( (resolve, reject) => {
+        setTimeout(() => {
+            res ? resolve ("promesa resuelta") : reject ("promesa rechazada")
+        }, 2000)
+    })
+}
+
+console.log(eventoFuturo3(true));
+
+//THEN AND CATCH
+
+eventoFuturo3(true)
+    .then ((response) => {
+        console.log(response); //promesa resuelta
+    })
+    .catch ( (error) => {
+        console.log(error);
+    })
+
+eventoFuturo3(false)
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally( () => {
+        console.log("FIN DEL PROCESO");
+    })
+
+    //EJEMPLO
+    //funcion que retorna tras3 segundo un array de objetos
+    const BD = [
+        {id: 1, nombre: "producto 1", precio: 1500},
+        {id: 2, nombre: "producto 2", precio: 1800},
+        {id: 3, nombre: "producto 3", precio: 1900}
+    ]
+
+    const pedirProductos = () => {
+        return new Promise ( (resolve, reject) => {
+            setTimeout( () => {
+                resolve(BD)
+            }, 3000)
+        })
+    }
+
+    //inicializamos con un array vacio
+    let product = []
+
+    const renderProductos = (arr) => {
+        //fuincion que genere la vista de los productos
+    }
+
+    //asincronicamente pedimos los datos y generamos la vista
+    pedirProductos ()
+    .then ((res) => {
+        product = res
+        renderProductos(product)
+    })
